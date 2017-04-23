82a98ec50692418a95b337cc5d23e2ca4a577aad
WinterCricket <hodhodhod2003@gmail.com>
2017-04-23 00:16:10 -0500

    Add random color selection

--- colorSchool/colorSchool.js
+++ colorSchool/colorSchool.js
@@ -7,10 +7,11 @@ var colors = [
 "rgb(0, 255,255)",
 ];
 var squares = document.querySelectorAll(".square");
-var targetColor = colors[4];
+var targetColor = targerColor();
 
 var rgbDisplay = document.querySelector("#rgbDisplay");
-// var messageDisplay = document.querySelector()
+var messageDisplay = document.querySelector("#message");
+
 rgbDisplay.textContent = targetColor;
 
 for(var i = 0; i < squares.length; i++){
@@ -23,9 +24,22 @@ for(var i = 0; i < squares.length; i++){
 		console.log(clickedColor +" " + " " +targetColor);
 		//compare color to target color
 		if(clickedColor === targetColor){
-			alert("Right!");
+			this.style.background
+			messageDisplay.textContent = "CORRECT! You Win!";
+			turnColors(clickedColor);
 		} else {
 			this.style.background = "#262626";
+			messageDisplay.textContent = "Try Again!";
 		}
 	});
+}
+
+function turnColors(color){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.background = targetColor;
+	}
+}
+function targetColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
