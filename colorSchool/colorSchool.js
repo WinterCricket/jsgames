42bbfb0902e4ddce6244b990a2465f4012974779
WinterCricket <hodhodhod2003@gmail.com>
2017-05-04 22:14:23 -0500

    add easy and hard buttons

--- colorSchool/colorSchool.js
+++ colorSchool/colorSchool.js
@@ -6,10 +6,34 @@ var rgbDisplay = document.querySelector("#rgbDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var hardBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function(){
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+});
+
+hardBtn.addEventListener("click", function(){
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+});
 
 resetButton.addEventListener("click", function(){
-	//when button clicked, page refreshed
-	alert("Clicked Reset Button");
+	
+	//generate all new colors, take from above
+	colors = generateRandomColors(6);
+	//pick new random color from array as target
+	targetedColor = targetColor();
+	//chanbe rgbDisplay to new targetedColor
+	rgbDisplay.textContent = targetedColor;
+	//change all squares to target color when correct
+	for(var i = 0; i < squares.length; i++){
+
+		squares[i].style.background = colors[i];
+	}
+	h1.style.background = "#262626";
 });
 rgbDisplay.textContent = targetedColor;
 
@@ -25,6 +49,7 @@ for(var i = 0; i < squares.length; i++){
 		if(clickedColor === targetedColor){
 			// this.style.background
 			messageDisplay.textContent = "CORRECT! You Win!";
+			resetButton.textContent = "Play Again?";
 			turnColors(clickedColor);
 			h1.style.background = clickedColor;
 		} else {
@@ -64,4 +89,6 @@ function randomColor(){
 	
 	return "rgb(" + r + ", " + g + ", " + b + ")";
 
-	
+}
+
+//check all basic lang
